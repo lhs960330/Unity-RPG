0284b310846898a8807e39ee86d277ce77611151
Lee Hyoseok <lhs19960330@gmail.com>
2024-02-28 15:11:02 +0900

    feat : 타격지정 변경

--- Assets/Scrpts/PlayerAttacker.cs
+++ Assets/Scrpts/PlayerAttacker.cs
@@ -5,9 +5,14 @@ using UnityEngine.InputSystem;
 public class PlayerAttacker : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    [SerializeField] Weapon weapon;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] float range;
+    [SerializeField] float attackangle;
+    [SerializeField] int damage;
+    //[SerializeField] Weapon weapon;
     private void Attack()
     {
+
         int rand = Random.Range(0, 2);
         if (rand == 0)
         {
@@ -16,17 +21,38 @@ public class PlayerAttacker : MonoBehaviour
         else { animator.SetTrigger("Attack2"); }
     }
 
-    public void EnableWeapon()
+    Collider[] colliders = new Collider[20];
+    private void AttackTiming()
     {
-        weapon.EnableWeapon();
+        int size = Physics.OverlapSphereNonAlloc(transform.position, range, colliders, layerMask);
+        for (int i = 0; i < size; i++)
+        {
+            Vector3 dirToTarger= (colliders[i].transform.position - transform.position).normalized;
+            // Angle은 연산이 느려지니 다른 방식을 사용해야된다.
+            if (Vector3.Angle(transform.forward, dirToTarger) > 90) continue;
+
+            IDamagable damagable = colliders[i].GetComponent<IDamagable>();
+            damagable?.TakeDamage(damage);
+
+        }
     }
-    public void DisableWeapon()
-    {
-        weapon.DisableWeapon();
-    }
+
+    /* public void EnableWeapon()
+     {
+         weapon.EnableWeapon();
+     }
+     public void DisableWeapon()
+     {
+         weapon.DisableWeapon();
+     }*/
     private void OnAttack(InputValue value)
     {
         Attack();
     }
-}
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
