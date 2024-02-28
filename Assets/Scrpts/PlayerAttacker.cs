4f9666dd0eee70569123faecd4698a188810be42
Lee Hyoseok <lhs19960330@gmail.com>
2024-02-28 15:34:54 +0900

    feat : 공격범위 지정(2)

--- Assets/Scrpts/PlayerAttacker.cs
+++ Assets/Scrpts/PlayerAttacker.cs
@@ -7,9 +7,17 @@ public class PlayerAttacker : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] LayerMask layerMask;
     [SerializeField] float range;
-    [SerializeField] float attackangle;
+    [SerializeField] float rangeAngle;
     [SerializeField] int damage;
     //[SerializeField] Weapon weapon;
+
+    private float cosRange;
+
+    private void Awake()
+    {
+        // 시작할때 먼저 계산 해놓는다.
+        cosRange = Mathf.Cos(rangeAngle * Mathf.Deg2Rad);
+    }
     private void Attack()
     {
 
@@ -27,9 +35,11 @@ public class PlayerAttacker : MonoBehaviour
         int size = Physics.OverlapSphereNonAlloc(transform.position, range, colliders, layerMask);
         for (int i = 0; i < size; i++)
         {
-            Vector3 dirToTarger= (colliders[i].transform.position - transform.position).normalized;
+            Vector3 dirToTarger = (colliders[i].transform.position - transform.position).normalized;
             // Angle은 연산이 느려지니 다른 방식을 사용해야된다.
-            if (Vector3.Angle(transform.forward, dirToTarger) > 90) continue;
+            // if (Vector3.Angle(transform.forward, dirToTarger) > rangeAngle) continue;
+            // 위에거 보다 훨씬 빠르다.
+            if (Vector3.Dot(transform.forward, dirToTarger) < cosRange) continue;
 
             IDamagable damagable = colliders[i].GetComponent<IDamagable>();
             damagable?.TakeDamage(damage);
